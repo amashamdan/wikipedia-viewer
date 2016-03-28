a8d3ac7e086360c8da47f580790a0faf3e302908
Amer Hamdan <amashamdan@gmail.com>
2016-03-28 23:43:38 +0200

    Style: Page is now styled.
    The page is styled, ready for use.
    jQuery slideDown effect is used when displaying search results.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
+/* Without the document.ready, the form submission doesn't work since the code might run
+before loading the DOM elements. */
 $(document).ready(function(){
+	
 	$(".search-form").submit(function(event){
 		event.preventDefault();
 		var wikiURL = 'https://en.wikipedia.org/w/api.php?action=opensearch&search='+
@@ -7,10 +10,12 @@ $(document).ready(function(){
 	        url: wikiURL,
 	        dataType: "jsonp",
 	        success: function(response) {
+	        	$(".results").append("<h3 class='reasults-header'>Search results:</h3>");
 	        	for (var i = 0; i < response[1].length; i++){
 	         		$(".results").append("<div class='result'><a href='" + response[3][i] + 
-	         			"' target='blank'><p>" + response[1][i] + "</p><p>" + response[2][i] + 
-	         			"</p></a>");
+	         			"' target='blank'><p class='result-title'><strong>" + response[1][i] + 
+	         			"</strong></p><p class='result-snippet'>" + response[2][i] + "</p></a>");
+	         		$(".result").slideDown();
 	         	}
 	        }
 	    });
